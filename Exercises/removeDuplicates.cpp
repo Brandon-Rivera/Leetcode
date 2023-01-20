dcab9530fa76df8c7a4af36ba00ed44d3b98077f
Brandon-Rivera <A01424338@itesm.mx>
2023-01-20 12:45:19 -0600

    Remove duplicates in place

--- Exercises/removeDuplicates.cpp
+++ Exercises/removeDuplicates.cpp
@@ -21,25 +21,58 @@ int removeDuplicates(vector<int> &nums)
     return nums.size();
 }
 
-void printVector( vector<int> vector){
+int removeDuplicatesInPlace(vector<int> &nums)
+{
+
+    int index = 1, uniqueElements = 1;
+
+    for (int i = 1; i < nums.size(); i++)
+    {
+        if (nums[i] != nums[i - 1])
+        {
+            nums[index] = nums[i];
+            index++;
+            uniqueElements++;
+        }
+        // Solution 1
+        // else
+        // {
+        //     nums.erase(nums.begin() + i);
+        //     i--;
+        // }
+    }
+
+    //Solution 2
+    for(int i = 0; i = uniqueElements - nums.size(); i++){
+        nums.pop_back();
+    }
+
+    return nums.size();
+}
+
+void printVector(vector<int> vector)
+{
 
     cout << "[ ";
-    for(int i = 0; i < vector.size(); i++){
-        if(vector[i] == vector[vector.size()-1]){
+    for (int i = 0; i < vector.size(); i++)
+    {
+        if (vector[i] == vector[vector.size() - 1])
+        {
             cout << vector[i] << " ";
-        } else {
+        }
+        else
+        {
             cout << vector[i] << ", ";
         }
     }
     cout << "] ";
-
 }
 
 int main()
 {
-    //vector<int> nums = {1,1,2};
-    vector<int> nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
-    cout << "Output: " << removeDuplicates(nums) << endl;
+    // vector<int> nums = {1,1,2};
+    // vector<int> nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+    cout << "Output: " << removeDuplicatesInPlace(nums) << endl;
     printVector(nums);
     return 0;
 }
--- Exercises/tempCodeRunnerFile.cpp
+++ Exercises/tempCodeRunnerFile.cpp
@@ -1,1 +1,4 @@
-arr
+  else
+        {
+            nums.erase(nums.begin() + i);
+        }

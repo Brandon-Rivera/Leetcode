7d367f78aabd50586c410c27bbec68f472432d57
Brandon-Rivera <A01424338@itesm.mx>
2023-01-20 12:08:46 -0600

    IDK

--- test.cpp
+++ test.cpp
@@ -5,11 +5,33 @@
 
 using namespace std;
 
-int main(){
+void printVector(vector<int> vector)
+{
+
+    cout << "[ ";
+    for (int i = 0; i < vector.size(); i++)
+    {
+        if (vector[i] == vector[vector.size() - 1])
+        {
+            cout << vector[i] << " ";
+        }
+        else
+        {
+            cout << vector[i] << ", ";
+        }
+    }
+    cout << "] ";
+}
+
+int main()
+{
     vector<int> a = {1, 2, 3, 4, 6};
     vector<string> b;
+    vector<int> sub_vec{a.begin() + 1, a.begin() + a.size()};
 
-    cout << a.size() << endl;
+    printVector(sub_vec);
+    cout << "\nMax Element = " << *max_element(sub_vec.begin(), sub_vec.end());
+
     return 0;
 }
 

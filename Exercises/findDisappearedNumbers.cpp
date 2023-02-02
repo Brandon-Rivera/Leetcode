a28079f0325ad5f8daee6dfba8235babbcd224da
Brandon-Rivera <A01424338@itesm.mx>
2023-02-02 09:39:56 -0600

    findDisappearedNumbers 2

--- Exercises/findDisappearedNumbers.cpp
+++ Exercises/findDisappearedNumbers.cpp
@@ -1,3 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void printVector(vector<int> vector)
+{
+
+    cout << "[ ";
+    for (int i = 0; i < vector.size(); i++)
+    {
+        if (i == vector.size() - 1)
+        {
+            cout << vector[i] << " ";
+        }
+        else
+        {
+            cout << vector[i] << ", ";
+        }
+    }
+    cout << "] ";
+}
+
 vector<int> findDisappearedNumbers(vector<int>& nums) {
         
         sort(nums.begin(), nums.end());
@@ -10,4 +33,11 @@ vector<int> findDisappearedNumbers(vector<int>& nums) {
         return ans;
         
     }
-};
+
+int main()
+{
+    vector<int> nums = {4,3,2,7,8,2,3,1};
+    vector<int> ans = findDisappearedNumbers(nums);
+    printVector(ans);
+    return 0;
+}
